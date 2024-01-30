a7985272811a55ea60c733a0fc1fe4a339677aa5
zhangfuwen <zhangfuwen@foxmail.com>
2024-01-30 15:54:11 +0800

    update lua test

--- app/src/main/java/cc/chenhe/lib/androidlua/demo/MainActivity.java
+++ app/src/main/java/cc/chenhe/lib/androidlua/demo/MainActivity.java
@@ -4,7 +4,12 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,18 +18,53 @@ import org.keplerproject.luajava.LuaStateFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 @SuppressLint("SetTextI18n")
 public class MainActivity extends AppCompatActivity {
 
+    class Wrapper {
+        public void showToast(Context context, String text) {
+            Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private LuaState lua = null;
     private TextView textView;
+    private TextView textView1;
+    private Spinner spinner;
+    private ArrayList<String> codeSnippets = new ArrayList<String>();
+    private Wrapper w = new Wrapper();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.text_view);
+        textView1 = findViewById(R.id.textView);
+        spinner = findViewById(R.id.spinner);
+
+
+        codeSnippets.add("return 'hello'");
+        codeSnippets.add("return obj:getCacheDir():toString()");
+        codeSnippets.add("return w.showToast(obj, 'hello')");
+
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item, codeSnippets);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//Setting the ArrayAdapter data on the Spinner
+        spinner.setAdapter(aa);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView tv = adapterView.findViewById(android.R.id.text1);
+                textView1.setText(tv.getText().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         lua = LuaStateFactory.newLuaState();
         if (lua == null) {
@@ -32,6 +72,10 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         lua.openLibs();
+        lua.pushJavaObject(this);
+        lua.setGlobal("obj");
+        lua.pushJavaObject(w);
+        lua.setGlobal("w");
         lua.LdoString(readAssetsTxt(this, "test.lua"));
 
         StringBuilder s = new StringBuilder();
@@ -50,9 +94,19 @@ public class MainActivity extends AppCompatActivity {
             lua.pop(1);
         });
         findViewById(R.id.btnInjectJavaFun).setOnClickListener(view -> new MyJavaFunction(lua).register());
+        textView1.setText("return getTime(' - passing by lua')");
         findViewById(R.id.btnCallInjectedFun).setOnClickListener(view -> {
-            lua.LdoString("return getTime(' - passing by lua')");
+            Toast.makeText(this, textView1.getText(), Toast.LENGTH_SHORT).show();
+            String code = textView1.getText().toString();
+            lua.LdoString(code);
             textView.setText(lua.toString(-1));
+            if(!codeSnippets.contains(code)) {
+                codeSnippets.add(code);
+            }
+            if (codeSnippets.size()> 10) {
+                // keep last 10
+                codeSnippets = (ArrayList<String>) codeSnippets.subList(codeSnippets.size() - 10, codeSnippets.size());
+            }
             lua.pop(1);
         });
         findViewById(R.id.btnGetLuaTable).setOnClickListener(view -> {
@@ -93,6 +147,8 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btnInjectJavaObj).setOnClickListener(view -> {
             lua.pushJavaObject(textView);
             lua.setGlobal("textView");
+            lua.pushJavaObject(this);
+            lua.setGlobal("obj");
             lua.pushInteger(Color.RED);
             lua.setGlobal("red");
             lua.LdoString("textView:setTextColor(red)");
